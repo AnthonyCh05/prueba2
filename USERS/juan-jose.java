324e72c9efa16cea2d044aaac7422d333c351738
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-03-24 19:56:24 -0500

    feat: Add JuanJoseUser Class with name and age atributes add method set name

---
+++ USERS/juan-jose.java
@@ -0,0 +1,13 @@
+public Class JuanJoseUser{
+    private String name;
+    private int age;
+
+    public JuanJoseUser(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+}
