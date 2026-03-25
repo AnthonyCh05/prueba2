79d3e9f3208188b05a94be788c9fcb3067d1708f
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-03-24 19:56:24 -0500

    feat: Add JuanJoseUser Class with name and age atributes

---
+++ USERS/juan-jose.java
@@ -0,0 +1,9 @@
+public Class JuanJoseUser{
+    private String name;
+    private int age;
+
+    public JuanJoseUser(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+}
